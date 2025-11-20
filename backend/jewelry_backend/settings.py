85d8305eb931a99c54152cc0ede9d68a6c31174a
mda-NebulaSynth <mda@nebulasynth.com>
2025-11-20 16:17:02 +0300

    .....

--- backend/jewelry_backend/settings.py
+++ backend/jewelry_backend/settings.py
@@ -68,13 +68,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jewelry_backend.wsgi.application'
 
 # Database
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
