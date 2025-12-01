216812f79eae7f802e80636f329b2a4e39eff43e
mda-NebulaSynth <mda@nebulasynth.com>
2025-12-01 09:56:23 +0300

    ....

--- backend/api/models.py
+++ backend/api/models.py
@@ -43,8 +43,8 @@ class Product(models.Model):
     
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    original_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    original_price = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
     category = models.CharField(max_length=20, choices=Category.choices)
     material = models.CharField(max_length=20, choices=Material.choices)
     availability = models.BooleanField(default=True)
@@ -120,7 +120,7 @@ class Order(models.Model):
     
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=15, decimal_places=2)
     
     # Shipping Address
     shipping_street = models.CharField(max_length=255)
@@ -145,7 +145,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(validators=[MinValueValidator(1)])
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
     
     def __str__(self):
         return f"{self.quantity}x {self.product.name}"
